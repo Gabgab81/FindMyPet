5e8be11b4dfa6abea168fcaf4523c2731bcd4d12
Gabriel Roux <rouxgab81@gmail.com>
2021-10-18 10:03:09 +0200

    fix config clooudinary

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# config/environments/development.rb
-config.active_storage.service = :cloudinary
